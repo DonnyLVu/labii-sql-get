ae758b79dbfe6129883304c7859a440f247d23ca
DonnyLVu <donnyvu897@gmail.com>
2020-11-03 14:01:02 -0800

    able to get data off api and display it

--- src/App.js
+++ src/App.js
@@ -1,43 +1,74 @@
 import './App.css';
 import React from 'react';
 import fetch from 'superagent';
-import AppRender from './AppRender.js';
+// import AppRender from './AppRender.js';
 
 export default class App extends React.Component {
   
   state = {
-      rangerData: '',
+    rangerData: [],
+    ranger_color:'',
+    favorite:'',
+    name:'',
+    order_appeared:'',
   }
 
 
   componentDidMount = async () => {
     const response = await fetch.get(`https://dry-castle-57746.herokuapp.com/rangers`);
-      this.setState({ rangerData: response.results });
+    console.log(response.body);
+      this.setState({ rangerData: response.body });
   }
 
-
+  
   render() {
     return (
-      <div>
-              {
-          <div className="rangerList"> 
-          {
-          this.props.rangerData.map(ranger => 
-          { return (
-          <AppRender 
-          uniqueId={ranger.id}
-          rangerName={ranger.name} 
-          ranger_color={ranger.ranger_color} 
-          favorite={ranger.favorite}
-          order_appeared={ranger.order_appeared}
-          ownerId={ranger.owner_id} />
-          )
-          })
-          }
+      <div className='rangerList'>
+        {
+        this.state.rangerData.map(ranger => 
+          <div>
+          <p className="name">Name: {ranger.name}</p>
+          <p className="rangerColor">Ranger Color: {ranger.ranger_color}</p>
+          <p className="favorite">Favorite Ranger?: {ranger.favorite.toString()}</p>
+          <p className="orderAppeared">Order of Appearance: {ranger.order_appeared}</p>
           </div>
-      }
-
+        )})
       </div>
     )
   }
 }
+
+// export default class App extends React.Component {
+  
+//   state = {
+//     rangerData: [],
+//   }
+
+
+//   componentDidMount = async () => {
+//     const response = await fetch.get(`https://dry-castle-57746.herokuapp.com/rangers`);
+//     console.log(response.body);
+//       this.setState({ rangerData: response.body });
+//   }
+
+
+//   render() {
+//     return (
+//       <div className='rangerList'>
+//         {
+//         this.state.rangerData.map(ranger => 
+//         { return (
+//         <AppRender 
+//         // uniqueId={ranger.unique_id}
+//         rangerName={ranger.name}
+//         rangerColor={ranger.ranger_color}
+//         favorite={ranger.favorite}
+//         orderAppeared={ranger.order_appeared}
+//         // ownerId={ranger.owner_id} 
+//         />
+//         )})
+//         }
+//       </div>
+//     )
+//   }
+// }
--- src/AppRender.js
+++ src/AppRender.js
@@ -4,12 +4,12 @@ export default class AppRender extends React.Component {
     render() {
         return (
             <div>
-                <p className="unique-id">{this.props.uniqueId}</p>
+                {/* <p className="uniqueId">UniqueId: {this.props.unique_id}</p> */}
                 <p className="name">Name: {this.props.name}</p>
-                <p className="ranger_color">Ranger Color: {this.props.ranger_color}</p>
-                <p className="favorite">FAvorite Ranger?: {this.props.favorite}</p>
-                <p className="order_appeared">Order of Appearance: {this.props.order_appeared}</p>
-                <p className="owner-id">Owner Id: {this.props.ownerId}</p>
+                <p className="rangerColor">Ranger Color: {this.props.ranger_color}</p>
+                <p className="favorite">Favorite Ranger?: {this.props.favorite}</p>
+                <p className="orderAppeared">Order of Appearance: {this.props.order_appeared}</p>
+                {/* <p className="ownerId">Owner Id: {this.props.owner_Id}</p> */}
             </div>
         )
     }
