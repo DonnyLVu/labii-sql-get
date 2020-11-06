5993c3289a75af86b8db38c653507d2f2e208b2e
DonnyLVu <donnyvu897@gmail.com>
2020-11-05 22:25:29 -0800

    did Fetch page, not done with DetailPage

---
+++ src/DetailPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { 
+    fetchColors, 
+    updateRanger, 
+    fetchRanger,
+} from './fetches.js';
+
+const userLocal = {
+    userId: 1
+};
+export default class DetailPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/Fetch.js
@@ -0,0 +1,65 @@
+import fetch from 'superagent';
+
+const URL = 'https://dry-castle-57746.herokuapp.com/';
+
+export async function fetchRangers() {
+    try {
+    const response = await fetch.get(`${URL}rangers`);
+        return response.body;
+    } catch(err) {
+        throw err;
+    }
+}
+
+export async function fetchRanger(newCreateRanger) {
+    try {
+        const response = await fetch.get(`${URL}rangers/${newCreateRanger}`);
+        return response.body;
+    } catch(err) {
+        throw err;
+    }
+}
+
+export async function fetchColors() {
+    try {
+        const response = await fetch.get(`${URL}colors`);
+        return response.body;
+    } catch(err) {
+        throw err;
+    }
+}
+
+export async function createRanger(newRangers) {
+    try {
+        await fetch
+        .post(`${URL}rangers`)
+        .send(newRangers);
+        
+        return;
+    } catch(err) {
+        throw err;
+    }
+}
+
+export async function updateRanger(newCreateRanger, newRangers) {
+    try {
+        await fetch
+        .put(`${URL}rangers/${newCreateRanger}`)
+        .send(newRangers);
+        
+        return;
+    } catch(err) {
+        throw err;
+    }
+}
+
+
+export async function deleteRangers(newCreateRanger) {
+    try {
+        await fetch.delete(`${URL}rangers/${newCreateRanger}`);
+        
+        return;
+    } catch(err) {
+        throw err;
+    }
+}
